82c14fb8886a13e28e4f55527cebf45890b1d250
Naresh Kumar <naresh.kumar@uniconvergetech.in>
2023-07-30 18:55:51 +0530

    First Commit - Leet Code Project Setup

---
+++ src/test/java/com/source/open/AppTest.java
@@ -0,0 +1,13 @@
+package com.source.open;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void demoTestMethod() {
+        assertTrue(true);
+    }
+}
