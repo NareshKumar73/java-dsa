89be6e833fc725315e11fe61d773c345e0936b6f
Naresh Kumar <naresh.kumar@uniconvergetech.in>
2023-07-31 11:18:18 +0530

    Added leetcode solution 1.Two Sum

--- src/main/java/com/source/open/App.java
+++ src/main/java/com/source/open/App.java
@@ -1,13 +1,12 @@
 package com.source.open;
 
 /**
- * Hello world!
- *
+ * @author Naresh Kumar
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    
+	public static void main(String[] args) {
+        System.out.println( "Hello DSA!" );
     }
 }
---
+++ src/main/java/com/source/open/leetcode/LC_0001_Two_Sum.java
@@ -0,0 +1,34 @@
+package com.source.open.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC_0001_Two_Sum {
+
+	// Complexity Analysis
+
+	// Time complexity: O(n).
+	// We traverse the list containing n elements only once.
+	// Each lookup in the table costs only O(1) time.
+
+	// Space complexity: O(n).
+	// The extra space required depends on the number of items stored in the hash
+	// table, which stores at most n elements.
+
+	public int[] twoSum(int[] nums, int target) {
+
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			int second = target - nums[i];
+
+			if (map.containsKey(second))
+				return new int[] { map.get(second), i };
+			else
+				map.put(nums[i], i);
+		}
+
+		return null;
+	}
+
+}
